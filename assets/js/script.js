336a6b3ab94d08cf4e29c6ed1d0b716fec80d3e9
vieiragcv <95590262+vieiragcv@users.noreply.github.com>
2022-02-05 18:22:28 -0500

    search handler function defined

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
 
 var locationTest = 'https://api.weatherapi.com/v1/current.json?key=f3315e9121504e948af175459220502&q=miami&aqi=yes'
-var searchedLocation = [];
-var miami = 'miami';
+var locationData = [];
+var locationDataTitle = ['Temperature:', 'Wind Speed:', 'UV Index:', 'Date:', 'Condition Image:', 'Condition:'];
 
 
 
-var currentSearchHandler = function (citySearch) {          // outputs the full API URL based search input
+
+var currentSearchHandler = function (citySearch) {          // receves city name as string ('miami') -> outputs the full API URL based search input
   var baseUrl = 'https://api.weatherapi.com/v1';
   var searchType = '/current';
   var key = '.json?key=f3315e9121504e948af175459220502';
@@ -15,28 +16,33 @@ var currentSearchHandler = function (citySearch) {          // outputs the full 
   return fullUrl;
 }
 
-var test = currentSearchHandler('miami');
+var apiURL = currentSearchHandler('miami');  // needs to become a function which captures text as submit button is clicked.
   
-fetch(test)
+fetch(apiURL)
 .then(function(response) {
   if(response.ok) {
     console.log('success');
     response.json().then(function(data) {
       console.log(data);
-      searchedLocation[0] = data.current.temp_f;           // temperature in F
-      searchedLocation[1] = data.current.wind_mph;         // wind speed
-      searchedLocation[2] = data.current.uv;               // uv
-      searchedLocation[3] = data.location.localtime;       // local date and time (at moment of search)
-      searchedLocation[4] = data.current.condition.icon;   // condition image
-      searchedLocation[5] = data.current.condition.text;   // condition text
+      locationData[0] = data.current.temp_f;           // temperature in F
+      locationData[1] = data.current.wind_mph;         // wind speed
+      locationData[2] = data.current.uv;               // uv
+      locationData[3] = data.location.localtime;       // local date and time (at moment of search)
+      locationData[4] = data.current.condition.icon;   // condition image
+      locationData[5] = data.current.condition.text;   // condition text
     });
   }
   else {
     alert('Error');
   }
-  console.log(searchedLocation);
+  console.log(locationData);
 });
 
+/* -------------------------------------------------------------------------------
+-                                 EVENT LISTENERS
+------------------------------------------------------------------------------- */
+
+// var userSearch = document.addEventListener('submit', functionNameTBD());
 
 
 
